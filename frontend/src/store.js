170e4043f3a85fc98b6826805a868952d6b57c95
Nasriddin <n.bektemirov@infosystems.uz>
2021-07-14 17:59:59 +0500

    adding cart is doing

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,14 +1,21 @@
 import {applyMiddleware, combineReducers, createStore} from 'redux'
 import thunk from "redux-thunk";
 import {composeWithDevTools} from "redux-devtools-extension";
-import {productListReducer} from "./reducers/productReducers";
+import {productDetailReducer, productListReducer} from "./reducers/productReducers";
+import {cartReducer} from "./reducers/cartReducers";
 
 
 const reducer = combineReducers({
-    productList: productListReducer
+    productList: productListReducer,
+    productDetails: productDetailReducer,
+    cart: cartReducer
 })
 
-const initialState = {}
+const cartItemsFormStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+
+const initialState = {
+    cart: {cartItems: cartItemsFormStorage}
+}
 
 const middleware = [thunk]
 
