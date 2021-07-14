ace46e4678a70ed4f4abfa7a85d59724ee66ff4e
Nasriddin <n.bektemirov@infosystems.uz>
2021-07-14 12:13:52 +0500

    added redux and added for products

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './bootstrap.min.css'
+import store from "./store";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -1,30 +1,33 @@
 import React, {useEffect, useState} from "react";
-import {Col, Container, Row} from "react-bootstrap";
+import {Alert, Col, Container, Row} from "react-bootstrap";
 import Product from "../components/Product";
-import axios from "axios";
+import {useDispatch, useSelector} from "react-redux";
+import {listProducts} from "../actions/productActions";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 
 const Home = () => {
-    const [products, setProducts] = useState([]);
+    const dispatch = useDispatch();
+
+    const productList = useSelector((state) => state.productList)
+
+    const {loading, error, products} = productList
 
     useEffect(() => {
-        const fetchProducts = async () => {
-            const {data} = await axios.get('api/products')
-
-            setProducts(data)
-        }
-        fetchProducts();
-    }, [])
-    return(
+        dispatch(listProducts());
+    }, [dispatch])
+    return (
         <>
             <Container>
                 <h1>Latest Products</h1>
-                <Row>
+                {loading ? (<Loader />) : error ? (<Message variant={'danger'}>{error}</Message>) : (<Row>
                     {products.map((product, index) => (
                         <Col sm={12} md={6} lg={4} xl={3} key={index}>
                             <Product product={product}/>
                         </Col>
                     ))}
-                </Row>
+                </Row>)}
+
             </Container>
         </>
     )
