334a162284931553b4b84779a2224076c4f89ae1
Nasriddin <n.bektemirov@infosystems.uz>
2021-08-04 18:44:08 +0500

    added user controller

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,22 +1,9 @@
 import express from "express";
-import expressAsyncHandler from "express-async-handler";
-import Product from "../models/productModel.js";
+import {getProduct, getProductById} from "../controllers/productController.js";
 
 const router = express.Router()
 
 
-router.get('/',expressAsyncHandler(async (req, res)=>{
-    const products = await Product.find({})
-    res.json(products)
-}) )
-router.get('/:id', expressAsyncHandler( async (req, res)=>{
-    const product = await Product.findById(req.params.id);
-    if (product){
-        res.json(product)
-    }else{
-        res.status(404).json({message: 'Product not fount'})
-    }
-    }
-
-))
+router.route('/').get(getProduct)
+router.route('/:id').get(getProductById)
 export default router
--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,23 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from "./config/db.js";
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 import {notFound, errorHandler} from "./middleware/errorMiddleware.js";
+import user from "./data/users";
 
 dotenv.config();
 
 connectDB();
 
 const app = express();
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('API is working...')
 });
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
