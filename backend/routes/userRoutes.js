f908919b7bd4e1a7e6e5a8391a1ce13a435f0860
Nasriddin <n.bektemirov@infosystems.uz>
2021-08-11 19:05:54 +0500

    finished authmiddlware and added getprofile and register

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { login } from "../controllers/userController.js";
+import { login, getUserProfile, registerUser } from "../controllers/userController.js";
+import { protect } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 
+router.route('/').post(registerUser)
 router.post('/login', login)
+router.route('/').get(protect, getUserProfile)
 
 export default router
